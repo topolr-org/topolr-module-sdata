e20391c5e19f4c6bf73ad5c36f4e92796244f282
Jinliang <hou80houzhu@vip.qq.com>
2017-04-20 14:43:49 +0800

    table

--- com/topolr/sdata/test.js
+++ com/topolr/sdata/test.js
@@ -7,40 +7,40 @@ Option({
     option:{
         override:{
             onendinit:function () {
-                // this.addChild({
-                //     type:"@table.simpletable",
-                //     option:{
-                //         url:sitePath+"/test/page",
-                //         cols:[
-                //             {name:"name",key:"name",width:100},
-                //             {name:"birth",key:"birth",width:100},
-                //             {name:"id",key:"id",width:100}
-                //         ]
-                //     },
-                //     container:this.finders("container")
-                // });
-                // this.addChild({
-                //     type:"@table.fntable",
-                //     option:{
-                //         url:sitePath+"/test/page",
-                //         cols:[
-                //             {name:"name",key:"name",width:100},
-                //             {name:"birth",key:"birth",width:100},
-                //             {name:"id",key:"id",width:100}
-                //         ],
-                //         checkbox: true,
-                //         num: true,
-                //         deals: [
-                //             {name:"A",action:"a"},
-                //             {name:"B",action:"b"},
-                //             {name:"C",action:"c"}
-                //         ],
-                //         tools:[
-                //             {name:"AA",action:"aa"}
-                //         ]
-                //     },
-                //     container:this.finders("container")
-                // });
+                this.addChild({
+                    type:"@table.simpletable",
+                    option:{
+                        url:sitePath+"/test/page",
+                        cols:[
+                            {name:"name",key:"name",width:100},
+                            {name:"birth",key:"birth",width:100},
+                            {name:"id",key:"id",width:100}
+                        ]
+                    },
+                    container:this.finders("container")
+                });
+                this.addChild({
+                    type:"@table.fntable",
+                    option:{
+                        url:sitePath+"/test/page",
+                        cols:[
+                            {name:"name",key:"name",width:100},
+                            {name:"birth",key:"birth",width:100},
+                            {name:"id",key:"id",width:100}
+                        ],
+                        checkbox: true,
+                        num: true,
+                        deals: [
+                            {name:"A",action:"a"},
+                            {name:"B",action:"b"},
+                            {name:"C",action:"c"}
+                        ],
+                        tools:[
+                            {name:"AA",action:"aa"}
+                        ]
+                    },
+                    container:this.finders("container1")
+                });
                 this.addChild({
                     type:"@table.doublefntable",
                     option:{
@@ -61,7 +61,7 @@ Option({
                             {name:"AA",action:"aa"}
                         ]
                     },
-                    container:this.finders("container")
+                    container:this.finders("container2")
                 });
             }
         }
